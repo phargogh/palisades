d2f6aa7401ecb139f28a7a67a4599ee7fb088d42
James Douglass <jdouglass@stanford.edu>
2013-09-24 15:14:23 -0700

    Update issue 2332
    Adding the timber json config file and now using that for my simple
    ApplicationTest at the moment.  It's a much simpler UI.

--- test/elements_test.py
+++ test/elements_test.py
@@ -10,4 +10,4 @@ IUI_CONFIG = os.path.join(TEST_DIR, 'data', 'iui_config')
 class ApplicationTest(unittest.TestCase):
     def test_build_application(self):
         elements.Application(os.path.join(IUI_CONFIG,
-            'pollination.json'))
+            'timber.json'))

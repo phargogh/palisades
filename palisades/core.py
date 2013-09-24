a39532ba927d83fe75c99201a4469a43cc798211
James Douglass <jdouglass@stanford.edu>
2013-09-24 10:54:06 -0700

    Update issue 2329
    Adding new files, migrated from my bitbucket repo.  This is everything
    I've worked on so far.

---
+++ palisades/core.py
@@ -0,0 +1,129 @@
+"""This file contains the core logic of the palisade package."""
+
+LAYOUT_VERTICAL_LIST = 1
+LAYOUT_HORIZONTAL_LIST = 2
+LAYOUT_GRID = 3
+
+
+class SignalNotFound(Exception):
+    """A custom exception for when a signal was not found."""
+    pass
+
+class Communicator(object):
+    """Element represents the base class for all UI elements.  It focuses
+    on inter-element connectivity and communication."""
+    # signals is a list of dictionaries
+    # signal['target'] - a pointer to the signal's target element and function
+    # signal['condition'] - the condition under which this signal is emitted
+    # When a signal is emitted, data about the signal should also be passed.
+    callbacks = []
+    
+    def register(self, callback):
+        """This function appends the target function call to the list of 
+        signals stored by this element"""
+        signal = {
+            'target': callback,
+            'condition': True,
+        }
+        self.callbacks.append(signal)
+        
+    def emit(self):
+        for signal in self.callbacks:
+            if signal['condition']:
+                signal['target']()
+                
+    def remove(self, target):
+        """"""
+        try:
+            callbacks_list = [cb['target'] for cb in self.callbacks]
+            index = callbacks_list.index(target)
+            self.callbacks.pop(index)
+        except ValueError:
+            # want to raise a custom exception here so that it's independent of
+            # implementation details in this class.
+            raise SignalNotFound(('Signal %s ' % str(target),
+                'was not found or was previously removed'))
+
+class UIElement(Communicator):
+    attributes = {}
+    value = None
+    enabled = True
+    args_id = None
+    
+    def set_value(self, new_value):
+# TODO: Is it better python form to raise an exception here instead of
+# just not doing anything?
+        if not self.is_enabled():
+            return
+
+        old_value = self.value    
+        self.value = new_value
+        if old_value != new_value:
+            self.emit()
+        
+    def get_value(self):
+        return self.value
+
+    def _set_enabled(self, enabled):
+        self.enabled = enabled
+        self.emit()
+        
+    def enable(self):
+        self._set_enabled(True)
+        
+    def disable(self):
+        self._set_enabled(False)
+       
+    def is_enabled(self):
+        return self.enabled
+       
+
+class Group(UIElement):
+    elements = []
+
+    def arguments(self):
+        output_args = {}
+        for element in self.elements:
+            try:
+                # call the function
+                output_args.update(element.arguments())
+            except AttributeError:
+                # the function did not exist, must have been a primitive
+                try:
+                    output_args[element.args_id] = element.value()
+                except AttributeError:
+                    # Args_id does not exist, so we don't care about its output value.
+                    pass
+        return output_args
+                    
+class Labeled(object):
+    label = u""
+
+    def set_label(self, new_label):
+        cast_label = new_label.decode("utf-8")
+        self.label = cast_label
+
+    def label(self):
+        return self.label
+
+class Text(UIElement, Labeled):
+    value = u""
+        
+    def set_value(self, new_value):
+        # enforce all strings to be utf-8
+        cast_value = new_value.decode('utf-8')
+        UIElement.set_value(self, cast_value)
+
+class File(Text):
+    pass
+  
+class OneShotForm(Group):
+# Special case group for a one-shot form submission, like what we
+# use with a model run.
+   pass
+    
+# This is the default element type registry, which will be a default
+# argument to the UI's registry object.
+ELEMENT_REGISTRY = {
+    'file': File,
+}
---
+++ palisades_test.py
@@ -0,0 +1,77 @@
+import unittest
+
+from palisades import core
+
+class CommunicatorTest(unittest.TestCase):
+    class SampleEmitter(core.Communicator):
+        def print_something(self):
+            print 'something printed'
+
+
+    def setUp(self):
+        self.a = core.Communicator()
+        self.b = self.SampleEmitter()
+    
+    def test_register(self):
+        self.a.register(self.b.print_something)
+        self.a.emit()
+
+    def test_remove(self):
+        self.a.register(self.b.print_something)
+        self.a.remove(self.b.print_something)
+       
+    def test_remove_fails(self):
+        self.a.register(self.b.print_something)
+        self.a.remove(self.b.print_something)
+        self.assertRaises(core.SignalNotFound, self.a.remove, self.b.print_something)
+
+class ElementTest(unittest.TestCase):
+    class Incrementer(object):
+        counter = 0
+        def increment(self):
+            self.counter += 1
+
+    def setUp(self):
+        self.counter = self.Incrementer()
+        self.element = core.UIElement()
+        
+    def test_set_value(self):
+        self.element.register(self.counter.increment)
+        self.element.set_value('aaa') # increment
+        self.element.set_value('aaa')  # no increment
+        self.element.set_value('b')  # increment
+
+        self.assertEqual(self.counter.counter, 2)
+
+    def test_is_enabled(self):
+        self.assertEqual(self.element.is_enabled(), True)
+        self.element.disable()
+        self.assertEqual(self.element.is_enabled(), False)
+        self.element.enable()
+        self.assertEqual(self.element.is_enabled(), True)
+       
+    def test_set_value_when_disabled(self):
+        self.assertEqual(self.element.is_enabled(), True)
+        self.element.set_value('aaa')
+        self.assertEqual(self.element.get_value(), 'aaa')
+        self.element.disable()
+        self.element.set_value('bbb')
+        self.assertEqual(self.element.get_value(), 'aaa')
+
+class FileTest(unittest.TestCase):
+    def setUp(self):
+        self.element = core.File()
+
+    def test_set_value_utf8(self):
+        # verify that if we set the value with a unicode string,
+        # we get a unicode string out
+        self.element.set_value(u'aaa')
+        returned_string = self.element.get_value()
+        self.assertEqual(type(returned_string), unicode)
+
+    def test_set_value_ascii(self):
+        #Verify that if we set the value with an ascii string,
+        # we get a unicode string out
+        self.element.set_value('aaa')
+        returned_string = self.element.get_value()
+        self.assertEqual(type(returned_string), unicode)

a58e97518a22a6d0dcdb50dee44ad0939a870b51
James Douglass <jdouglass@stanford.edu>
2013-09-25 14:58:42 -0700

    Update issue 2333
    Reorganizing the ui module.

--- palisades/ui.py
+++ palisades/ui.py
@@ -1,34 +1,36 @@
 from PyQt4 import QtGui
 from PyQt4 import QtCore
-import Tkinter
 
-class Qt4():
-    class Application():
-        app = QtGui.QApplication()
+class Application():
+    app = QtGui.QApplication([''])
 
-    class Empty(QtGui.QWidget):
-        pass
+class Empty(QtGui.QWidget):
+    pass
 
-    class ValidationButton(QtGui.QPushButton):
-        pass
+class ValidationButton(QtGui.QPushButton):
+    pass
 
-    class Label(QtGui.QLabel):
-        pass
+class Label(QtGui.QLabel):
+    pass
 
-    class TextField(QtGui.QLineEdit):
-        pass
+class TextField(QtGui.QLineEdit):
+    pass
 
-    class FileButton(QtGui.QPushButton):
-        pass
+class FileButton(QtGui.QPushButton):
+    pass
 
-    class HelpButton(QtGui.QPushButton)
+class HelpButton(QtGui.QPushButton):
+    pass
 
-    class Text():
-        _validation_button = ValidationButton()
-        _label = Label()
-        _text_field = TextField()
-        _help_button = HelpButton()
-        elements = [
+class Text():
+    _validation_button = ValidationButton()
+    _label = Label()
+    _text_field = TextField()
+    _help_button = HelpButton()
+    elements = []
+
+    def __init__(self):
+        self.elements = [
             self._validation_button,
             self._label,
             self._text_field,
@@ -36,9 +38,11 @@ class Qt4():
             self._help_button
         ]
 
-    class File():
-        _file_button = FileButton()
-        elements = [
+class File():
+    _file_button = FileButton()
+
+    def __init__(self):
+        self.elements = [
             self._validation_button,
             self._label,
             self._text_field,

db48f064357b75fbd9f1348e72a04f7d2545fdec
James Douglass <jdouglass@stanford.edu>
2013-10-02 15:40:50 -0700

    Update issue 2359
    Logging is now only saved to a log file if the log file name is provided
    as a parameter to the executor.

--- palisades/execution.py
+++ palisades/execution.py
@@ -17,23 +17,28 @@ class ThreadFilter(logging.Filter):
         return False
 
 class Executor(threading.Thread):
-    def __init__(self, module, args, func_name='execute'):
+    def __init__(self, module, args, func_name='execute', log_file=None):
         threading.Thread.__init__(self)
         self.module = module
         self.args = args
         self.func_name = func_name
+        self.log_file = log_file
 
-        # Set up the log file writing here as a logging FileHandler.
-        log_fmt = "%(asctime)s %(name)-18s %(levelname)-8s %(message)s"
-        date_fmt = "%m/%d/%Y %H:%M:%S "
-        formatter = logging.Formatter(log_fmt, date_fmt)
-        self.fh = logging.FileHandler('log-%s.foo' % self.name)
-        self.fh.setFormatter(formatter)
-        self.fh.addFilter(ThreadFilter(self.name))
-        LOGGER.addHandler(self.fh)
+        # If the user provided a log file URI, then we save the log file there.
+        # If no log file provided, skip saving to a log file.
+        if log_file is not None:
+            # Set up the log file writing here as a logging FileHandler.
+            log_fmt = "%(asctime)s %(name)-18s %(levelname)-8s %(message)s"
+            date_fmt = "%m/%d/%Y %H:%M:%S "
+            formatter = logging.Formatter(log_fmt, date_fmt)
+            self.fh = logging.FileHandler(log_file)
+            self.fh.setFormatter(formatter)
+            self.fh.addFilter(ThreadFilter(self.name))
+            LOGGER.addHandler(self.fh)
 
     def __del__(self):
-        LOGGER.removeHandler(self.fh)
+        if self.log_file is not None:
+            LOGGER.removeHandler(self.fh)
 
     def run(self):
         function = getattr(self.module, self.func_name)
--- test/execution_test.py
+++ test/execution_test.py
@@ -28,7 +28,18 @@ class ExecutorTest(unittest.TestCase):
     def test_with_logging(self):
         module = imp.load_source('sample', os.path.join(DATA_DIR,
             'sample_scripts.py'))
-        executor = execution.Executor(module, None, func_name='try_logging')
+
+        temp_file_uri = os.path.join(DATA_DIR, 'test_log.txt')
+        executor = execution.Executor(module, None, func_name='try_logging',
+            log_file=temp_file_uri)
         executor.start()
         executor.join()
         LOGGER.debug('hello.  This should not appear in the log file.')
+
+        with open(temp_file_uri) as file_obj:
+            for i, line in enumerate(file_obj):
+                pass
+        num_lines = i + 1
+
+        self.assertEqual(num_lines, 2)
+        os.remove(temp_file_uri)

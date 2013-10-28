5717a844ff070b7ee1ca7bfdf75dee3f74a85668
James Douglass <jdouglass@stanford.edu>
2013-10-28 15:43:17 -0700

    Update issue 2426
    Adding the start to a test file for gui_core

---
+++ test/gui_core_test.py
@@ -0,0 +1,20 @@
+import unittest
+import mock
+
+import palisades
+from palisades import elements
+from palisades.gui import core
+
+class TextGUITest(unittest.TestCase):
+    def setUp(self):
+        self.application = core.ApplicationGUI()
+        self.core_element = elements.Text({})
+        self.gui = core.TextGUI(self.core_element)
+
+    def test_set_value(self):
+        # verify that when the textfield's value is changed, the element's
+        # set_value function is emitted.
+        new_value = 'hello world!'
+        self.gui._text_field.value_changed.emit(new_value)
+        self.assertEqual(self.core_element.value(), new_value)
+

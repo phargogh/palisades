83cf90a2f2039a8db21c12c689107039563020f1
James Douglass <jdouglass@stanford.edu>
2013-10-28 15:44:49 -0700

    Update issue 2426
    Adding another assertion to the set_value test to ensure that the
    current value of the core element is not the new value.

--- test/gui_core_test.py
+++ test/gui_core_test.py
@@ -15,6 +15,7 @@ class TextGUITest(unittest.TestCase):
         # verify that when the textfield's value is changed, the element's
         # set_value function is emitted.
         new_value = 'hello world!'
+        self.assertNotEqual(self.core_element.value(), new_value)
         self.gui._text_field.value_changed.emit(new_value)
         self.assertEqual(self.core_element.value(), new_value)
 

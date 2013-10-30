fe94ae3ca3ed1fabea58dd8245356f0f04db3d8d
James Douglass <jdouglass@stanford.edu>
2013-10-30 11:04:10 -0700

    Update issue 2426
    Adding a test class for an integration test of the File element/gui/qt

--- test/gui_integration_test.py
+++ test/gui_integration_test.py
@@ -1,6 +1,8 @@
 import unittest
+import os
+import time
 
-import PyQt4.QtTest as QtTest
+from PyQt4.QtTest import QTest
 
 from palisades import elements
 from palisades.gui import core
@@ -52,3 +54,79 @@ class QtTextIntegrationTest(unittest.TestCase):
 
         # check the validation state of the GUI.
         self.assertEqual(self.gui._validation_button.error_state, 'pass')
+
+class QtFileIntegrationTest(unittest.TestCase):
+    def setUp(self):
+        config = {
+            'defaultValue':  os.getcwd(),
+            'validateAs': {
+                'type': 'folder',
+                'mustExist': True,
+            }
+        }
+        self.element = elements.File(config)
+        self.gui = core.FileGUI(self.element)
+
+    def test_set_text(self):
+        """Assert element's value when text field value is changed."""
+        # assert that the starting value is what we set in the config.
+        self.assertEqual(self.element.value(), os.getcwd())
+
+        new_value = os.path.join(os.getcwd(), 'test')
+        self.gui._text_field.setText(new_value)
+        self.assertEqual(self.element.value(), new_value)
+
+        new_value = os.path.join(os.getcwd(), 'palisades')
+        self.gui._text_field.setText(new_value)
+        self.assertEqual(self.element.value(), new_value)
+
+    def test_validation(self):
+        """Assert that validation takes place when text is changed."""
+
+        self.assertEqual(self.element.value(), os.getcwd())
+
+        # check the validation state to start out
+        self.assertEqual(self.gui._validation_button.error_state, 'pass')
+        self.assertEqual(self.element.is_valid(), True)
+
+        # set the textfield value to something else.
+        new_value = os.path.join(os.getcwd(), 'test')
+        self.element.set_value(new_value)
+        self.assertEqual(self.element.value(), new_value)
+        self.assertRaises(RuntimeError, self.element._validator.thread.start)
+        QTest.qWait(100)
+        self.assertEqual(self.element.is_valid(), True)
+
+        # set the textfield value to something that we know will fail validation
+        # Need to first verify that the folder that shouldn't exist doesn't.
+        new_value = os.path.join(os.getcwd(), 'nonexistent_dir')
+        self.assertEqual(os.path.exists(new_value), False)
+        self.element.set_value(new_value)
+        self.assertRaises(RuntimeError, self.element._validator.thread.start)
+
+        # given the new value, verify that the element failed validation.
+        QTest.qWait(100)
+        self.assertEqual(self.element.is_valid(), False)
+
+        # verify that the validation button displays the correct error state.
+        self.assertEqual(self.gui._validation_button.error_state, 'error')
+        self.assertEqual(self.gui._validation_button.isEnabled(), True)
+        self.assertNotEqual(self.gui._validation_button.error_text, '')
+        self.assertEqual(self.gui._validation_button.isFlat(), False)
+
+    def test_file_button(self):
+        """Assert that file selection sets the element value, starts validation"""
+        self.assertEqual(self.element.value(), os.getcwd())
+
+        # create a new path and emit the file_selected signal with it.
+        # can't actually simulate the mousebutton click here or emit the Qt
+        # signal because it will cause the program to present a Qt file dialog
+        # and block the program on user input from the file window.
+        new_path = os.path.join(os.getcwd(), 'test')
+        self.gui._file_button.file_selected.emit(new_path)
+
+        # verify that the new path has been set as the element value and is also
+        # the value of the text field.
+        QTest.qWait(100)
+        self.assertEqual(str(self.gui._text_field.text()), new_path)
+        self.assertEqual(self.element.value(), new_path)

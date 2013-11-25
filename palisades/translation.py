30d5698e6d3716a211a8d0a629f06b2c49d15363
James Douglass <jdouglass@stanford.edu>
2013-11-25 14:26:17 -0800

    Update issue 2505
    Adding a docstring to the translate_config function.

--- palisades/translation.py
+++ palisades/translation.py
@@ -6,6 +6,88 @@ import palisades.utils
 
 # assume per-attribute translation
 def translate_config(config, lang_code, extra_keys=[]):
+    """Translate a dictionary containing element configuration options.  Any
+        keys that are not translated are left untouched.
+
+        config - a python dictionary contained configuration options
+        lang_code - a python language code matching the language to translate
+            to.  This absolutely must match the language identifier in the
+            configuration.  See below for example configuration.
+        extra_keys=[] - keys to be translated.  Default keys translated are:
+            ['label', 'modelName', 'helpText']
+
+        ========================
+        A simple configuration dictionary such as:
+        {
+            'id': 'sample_element',
+            'label': {
+                'en': 'hello world!',
+                'de': 'Hallo, Weld!',
+                'es': u'¡Hola, mundo!',
+            },
+        }
+
+        will translate to this when the 'de' language code is used:
+        {
+            'id': 'sample_element',
+            'label': 'Hallo, Weld!',
+        }
+
+        The same dictionary will translate to this when 'es' is the input
+        language code:
+        {
+            'id': 'sample_element',
+            'label': u'¡Hola, mundo!',
+        }
+
+        ========================
+        A more complicated configuration dictionary:
+        {
+            'id': 'sample_element',
+            'label': {
+                'en': 'hello world!',
+                'de': 'Hallo, Weld!',
+                'es': u'¡Hola, mundo!',
+            },
+            'elements': [
+                {
+                    'id': 'element_1',
+                    'label': {
+                        'en': 'element one',
+                        'de': 'das Element eins',
+                        'es': 'elemento uno',
+                    }
+                },
+                {
+                    'id': 'element_2',
+                    'label': {
+                        'en': 'element two',
+                        'de': 'das Element zwei',
+                        'es': 'elemento dos',
+                    }
+                }
+            ]
+        }
+
+        This will translate to German as so:
+        {
+            'id': 'sample_element',
+            'label': 'Hallo, Weld!',
+            'elements': [
+                {
+                    'id': 'element_1',
+                    'label': 'das Element eins',
+                },
+                {
+                    'id': 'element_2',
+                    'label': 'das Element zwei',
+                }
+            ]
+        }
+
+
+        returns a python dictionary."""
+
     translated_config = config.copy()
 
     translateable_keys = ['label', 'modelName', 'helpText'] + extra_keys

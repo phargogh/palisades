f8c46d0aded1f8358ddb5524007c456db7c739d7
James Douglass <jamesdouglassusa@gmail.com>
2014-07-15 11:57:31 -0700

    Refactoring version querying
    Trying to make it easier to have both mercurial and git capabilities.

--- palisades/versioning.py
+++ palisades/versioning.py
@@ -7,21 +7,107 @@ import subprocess
 import tempfile
 import time
 
-HG_CALL = 'hg log -r . --config ui.report_untrusted=False'
+if platform.system() != 'Windows':
+    from shutil import WindowsError
 
 LOGGER = logging.getLogger('versioning')
 LOGGER.setLevel(logging.ERROR)
 
-if platform.system() != 'Windows':
-    from shutil import WindowsError
+class VCSQuerier(object):
+    def _run_command(self, cmd):
+        """Run a subprocess.Popen command.  This function is intended for internal
+        use only and ensures a certain degree of uniformity across the various
+        subprocess calls made in this module.
+
+        cmd - a python string to be executed in the shell.
+
+        Returns a python bytestring of the output of the input command."""
+        p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        return p.stdout.read()
+
+    @property
+    def tag_distance(self):
+        pass
+
+    @property
+    def build_id(self):
+        pass
+
+    @property
+    def latest_tag(self):
+        pass
+
+    @property
+    def py_arch(self):
+        """This function gets the python architecture string.  Returns a string."""
+        return platform.architecture()[0]
+
+    @property
+    def release_version(self):
+        """This function gets the release version.  Returns either the latest tag
+        (if we're on a release tag) or None, if we're on a dev changeset."""
+        if self.tag_distance == 0:
+            return self.latest_tag
+        return None
+
+    @property
+    def version(self):
+        """This function gets the module's version string.  This will be either the
+        dev build ID (if we're on a dev build) or the current tag if we're on a
+        known tag.  Either way, the return type is a string."""
+        release_version = self.release_version
+        if release_version == None:
+            return self.build_dev_id(self.build_id)
+        return release_version
+
+    def build_dev_id(self, build_id=None):
+        """This function builds the dev version string.  Returns a string."""
+        if build_id == None:
+            build_id = self.build_id
+        return 'dev%s' % (build_id)
+
+    def get_architecture_string(self):
+        """Return a string representing the operating system and the python
+        architecture on which this python installation is operating (which may be
+        different than the native processor architecture.."""
+        return '%s%s' % (platform.system().lower(),
+            platform.architecture()[0][0:2])
+
+
+class HgRepo(VCSQuerier):
+    HG_CALL = 'hg log -r . --config ui.report_untrusted=False'
+
+    @property
+    def build_id(self):
+        """Call mercurial with a template argument to get the build ID.  Returns a
+        python bytestring."""
+        cmd = self.HG_CALL + ' --template "{latesttagdistance}:{latesttag} [{node|short}]"'
+        return self._run_command(cmd)
+
+    @property
+    def tag_distance(self):
+        """Call mercurial with a template argument to get the distance to the latest
+        tag.  Returns an int."""
+        cmd = self.HG_CALL + ' --template "{latesttagdistance}"'
+        return int(self._run_command(cmd))
+
+    @property
+    def latest_tag(self):
+        """Call mercurial with a template argument to get the latest tag.  Returns a
+        python bytestring."""
+        cmd = self.HG_CALL + ' --template "{latesttag}"'
+        return self._run_command(cmd)
+
+REPO = HgRepo()
 
 def build_data():
     """Returns a dictionary of relevant build data."""
     data = {
-        'release': get_latest_tag(),
-        'build_id': get_build_id(),
-        'py_arch': get_py_arch(),
-        'version_str': version()
+        'release': REPO.latest_tag,
+        'build_id': REPO.build_id,
+        'py_arch': REPO.py_arch,
+        'version_str': REPO.version
     }
     return data
 
@@ -34,7 +120,7 @@ def write_build_info(source_file_uri):
     source_file = open(os.path.abspath(source_file_uri))
     for line in source_file:
         if line == "__version__ = 'dev'\n":
-            temp_file.write("__version__ = '%s'\n" % version())
+            temp_file.write("__version__ = '%s'\n" % REPO.version())
         elif line == "build_data = None\n":
             build_information = build_data()
             temp_file.write("build_data = %s\n" % str(build_information.keys()))
@@ -88,74 +174,4 @@ def _temporary_filename():
     return path
 
 
-def get_py_arch():
-    """This function gets the python architecture string.  Returns a string."""
-    return platform.architecture()[0]
 
-def get_release_version():
-    """This function gets the release version.  Returns either the latest tag
-    (if we're on a release tag) or None, if we're on a dev changeset."""
-    if get_tag_distance() == 0:
-        return get_latest_tag()
-    return None
-
-def version():
-    """This function gets the module's version string.  This will be either the
-    dev build ID (if we're on a dev build) or the current tag if we're on a
-    known tag.  Either way, the return type is a string."""
-    release_version = get_release_version()
-    if release_version == None:
-        return build_dev_id(get_build_id())
-    return release_version
-
-def build_dev_id(build_id=None):
-    """This function builds the dev version string.  Returns a string."""
-    if build_id == None:
-        build_id = get_build_id()
-    return 'dev%s' % (build_id)
-
-def get_architecture_string():
-    """Return a string representing the operating system and the python
-    architecture on which this python installation is operating (which may be
-    different than the native processor architecture.."""
-    return '%s%s' % (platform.system().lower(),
-        platform.architecture()[0][0:2])
-
-def get_version_from_hg():
-    """Get the version from mercurial.  If we're on a tag, return that.
-    Otherwise, build the dev id and return that instead."""
-    # TODO: Test that Hg exists before getting this information.
-    if get_tag_distance() == 0:
-        return get_latest_tag()
-    else:
-        return build_dev_id()
-
-def get_build_id():
-    """Call mercurial with a template argument to get the build ID.  Returns a
-    python bytestring."""
-    cmd = HG_CALL + ' --template "{latesttagdistance}:{latesttag} [{node|short}]"'
-    return run_command(cmd)
-
-def get_tag_distance():
-    """Call mercurial with a template argument to get the distance to the latest
-    tag.  Returns an int."""
-    cmd = HG_CALL + ' --template "{latesttagdistance}"'
-    return int(run_command(cmd))
-
-def get_latest_tag():
-    """Call mercurial with a template argument to get the latest tag.  Returns a
-    python bytestring."""
-    cmd = HG_CALL + ' --template "{latesttag}"'
-    return run_command(cmd)
-
-def run_command(cmd):
-    """Run a subprocess.Popen command.  This function is intended for internal
-    use only and ensures a certain degree of uniformity across the various
-    subprocess calls made in this module.
-
-    cmd - a python string to be executed in the shell.
-
-    Returns a python bytestring of the output of the input command."""
-    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return p.stdout.read()

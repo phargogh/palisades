e0411a18ec4c87bdb6296cf97061a17fd37a7b37
James Douglass <jamesdouglassusa@gmail.com>
2014-07-17 13:19:09 -0700

    Commenting out cached run menu option

--- palisades/gui/qt4.py
+++ palisades/gui/qt4.py
@@ -947,7 +947,7 @@ class FormWindow(QtWidget, QtGui.QWidget):
         self.load_file_action.setShortcut(QtGui.QKeySequence("Ctrl+O"))
         self.save_file_action = self.file_menu.addAction('&Save parameters ...')
         self.save_file_action.setShortcut(QtGui.QKeySequence("Ctrl+S"))
-        self.remove_lastrun = self.file_menu.addAction('&Clear cached runs ...')
+#        self.remove_lastrun = self.file_menu.addAction('&Clear cached runs ...')
         self.exit_action = self.file_menu.addAction('Exit')
         self.exit_action.setShortcut(QtGui.QKeySequence("Ctrl+Q"))
         self.menu_bar.addMenu(self.file_menu)

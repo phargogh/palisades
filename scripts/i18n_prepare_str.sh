557ef8a87e57fac26c0e480d79a0fb64e73a2c8b
James Douglass <jamesdouglassusa@gmail.com>
2014-08-04 14:17:58 -0700

    Adding a script to make translation merging easier
    Script merges the existing translated .po file with the new .pot file
    for the package, saving the output to a temp file.  Then, we copy back
    the temp file to the original location.

---
+++ scripts/i18n_prepare_str.sh
@@ -0,0 +1,18 @@
+# Convert Palisades.pot to the language-specific .po files, merging them with
+# the older versions of the same file.
+
+pushd i18n
+mkdir merged_po
+
+for locale in "es" "en" "de"
+do
+    # merge the new file with the existing one, saving to the merged_po folder
+    msgmerge $locale.po Palisades.pot > merged_po/$locale.po
+
+    # copy the merged_po file to the original location.
+    cp merged_po/$locale.po $locale.po
+done
+
+rm -r merged_po
+popd
+

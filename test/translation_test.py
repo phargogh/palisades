e379f7b817577eff45f913d8f22d57be5aaf04a3
James Douglass <jamesdouglassusa@gmail.com>
2015-10-02 16:12:04 -0700

    Adding tests for new complete-translation detection

--- test/translation_test.py
+++ test/translation_test.py
@@ -169,3 +169,60 @@ class TrivialTranslationTest(unittest.TestCase):
         }
         self.assertEqual(palisades.i18n.translation.translate_config(config, 'de'),
             expected_german)
+
+class CompleteLanguageDetection(unittest.TestCase):
+    def test_dict_detection(self):
+        """Language code detection on trivial config dictionary."""
+        user_dict = {
+            'label': {
+                'en': 'foo',
+                'es': 'foo',
+                'de': 'foo',
+            }
+        }
+        self.assertEqual(palisades.i18n.translation.fetch_allowed_translations(
+            user_dict), ['de', 'en', 'es'])
+
+    def test_dict_with_elements_detection(self):
+        """Language code detection with nested elements list."""
+        user_dict = {
+            'label': {
+                'en': 'foo',
+                'es': 'foo',
+                'de': 'foo',
+            },
+            'elements': [
+                {
+                    'label': {
+                        'en': 'bar',
+                        'es': 'bar',
+                        'de': 'bar',
+                    }
+                }
+            ]
+        }
+        self.assertEqual(palisades.i18n.translation.fetch_allowed_translations(
+            user_dict), ['de', 'en', 'es'])
+
+    def test_dict_with_palisades_unsupported_lang(self):
+        """When a user lang is unsupportedi by palisades"""
+        user_dict = {
+            'label': {
+                'foo': 'this is an unsupported language',
+                'es': 'supported',
+                'de': 'supported',
+            }
+        }
+        self.assertEqual(palisades.i18n.translation.fetch_allowed_translations(
+            user_dict), ['de', 'es'])
+
+    def test_dict_with_user_keys(self):
+        """When a user defines extra translateable keys"""
+        user_dict = {
+            'extra_key': {
+                'es': 'supported',
+                'de': 'supported',
+            }
+        }
+        self.assertEqual(palisades.i18n.translation.fetch_allowed_translations(
+            user_dict, extra_keys=['extra_key']), ['de', 'es'])
